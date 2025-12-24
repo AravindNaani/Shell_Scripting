7929a1525aafc44fac5242931d3115490101ca42
Aravind <aravindbujji7@gmail.com>
2025-12-24 20:55:34 +0530

    Fix server_status_check.sh to remove 'local' keyword from STATUS variable declaration

--- server_status_check.sh
+++ server_status_check.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 for HOSTNAME in google.com facebook.com linkedin.com
 do 
-    local STATUS=$(ping -c 1 $HOSTNAME)
+    STATUS=$(ping -c 1 $HOSTNAME)
     if [ -n $STATUS ]
     then
         echo "$HOSTNAME is UP"
