aa15fbef96bae36e97fce5546c86286932bd25d5
Aravind <aravindbujji7@gmail.com>
2025-12-24 19:20:26 +0530

    Fix exit codes in disk_usage_checker.sh for better error handling

--- disk_usage_checker.sh
+++ disk_usage_checker.sh
@@ -7,9 +7,12 @@ DISK_USAGE=$(df -h / | awk  'NR==2 {gsub("%","",$5); print $5}')
 if [ "$DISK_USAGE" -ge 90 ]
 then 
     echo "[CRITICAL] : Disk Usage is $DISK_USAGE - VERY HIGH"
+    exit 2
 elif [ "$DISK_USAGE" -gt 80 ]
 then 
     echo "[WARNING] : Disk Usage is $DISK_USAGE - HIGH"
+    exit 1
 else
     echo "[OK] : Disk Usage is $DISK_USAGE - OK"
+    exit 0
 fi
