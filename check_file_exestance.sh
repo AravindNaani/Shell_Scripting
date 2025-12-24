67ed2eb1eda4c127953fc14e6267b59cb77307d0
Aravind <aravindbujji7@gmail.com>
2025-12-24 20:40:58 +0530

    Fix check_file_exestance.sh to validate directory existence and search for specified file

--- check_file_exestance.sh
+++ check_file_exestance.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 set -euo pipefail
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then 
     echo "Usage : $0 <file Name>"
 fi
 
 FILE=$1
+FILE_PATH=$2
 
-if [ -f "$FILE" ]
+if [ ! -d "$FILE_PATH" ]
 then
-    echo "$FILE available in home directory"
+    echo "[ERROR] Directory $FILE_PATH is not exists"
+    exit 1
+fi
+
+IS_FILE_FOUND=$(find "$FILE_PATH" -type f -name "$FILE" 2>/dev/null)
+
+if [ -n "IS_FILE_FOUND" ]
+then
+    echo "[OK] $FILE available in $FILE_PATH directory"
     exit 0
 else
-    echo "$FILE is not available in home directory"
+    echo "[ERROR] $FILE is not available in $FILE_PATH directory"
     exit 1
 fi
